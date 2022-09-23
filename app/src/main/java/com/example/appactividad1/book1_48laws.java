ecd1668a60fe4bec1862df7a762431d6014cd9c6
Arjun <arjunjot18@gmail.com>
2022-09-23 15:52:33 +0200

    1

---
+++ app/src/main/java/com/example/appactividad1/book1_48laws.java
@@ -0,0 +1,14 @@
+package com.example.appactividad1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class book1_48laws extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book148laws);
+    }
+}
